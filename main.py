4c60adf83ea46c85f72a0ab16a8bd3ef7d7cbaad
Siddharth-Goutham <siddhartha.nalluraya@gmail.com>
2026-03-15 00:57:43 +0530

    removed the lazy_load() because I could do it directly and added text_splitter

--- main.py
+++ main.py
@@ -2,10 +2,14 @@
 # ---------------------------------------
 
 from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 file_path= "policy.pdf"
 loader=PyPDFLoader(file_path)
 
 docs=loader.lazy_load()
-for document in docs:
-    print(document)
+
+
+
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=100)
+texts = text_splitter.split_documents(docs)
