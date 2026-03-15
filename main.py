784d7994f25193b45a3032ccf120f7dec848ba2e
Siddharth-Goutham <siddhartha.nalluraya@gmail.com>
2026-03-15 13:23:51 +0530

    added embedding and module called hugging face embeddings

--- main.py
+++ main.py
@@ -3,13 +3,20 @@
 
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_huggingface import HuggingFaceEmbeddings
 
 file_path= "policy.pdf"
 loader=PyPDFLoader(file_path)
 
-docs=loader.lazy_load()
+docs=loader.load()
 
 
 
 text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=100)
 texts = text_splitter.split_documents(docs)
+print(texts)
+
+embeddings = HuggingFaceEmbeddings(
+                model_name="sentence-transformers/all-mpnet-base-v2"
+                )
+
