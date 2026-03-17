965a809619c10d3a74c3647ec65c95e4fe243c0d
mokshsalecha68-del <mokshsalecha68@gmail.com>
2026-03-17 17:04:14 +0530

    Update pdf_service.py

--- backend/app/services/pdf_service.py
+++ backend/app/services/pdf_service.py
@@ -1,1 +1,13 @@
+from langchain_community.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 
+def process_pdf(file_path: str):
+    loader = PyPDFLoader(file_path)
+    documents = loader.load()
+
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size=500,
+        chunk_overlap=100
+    )
+
+    return splitter.split_documents(documents)
