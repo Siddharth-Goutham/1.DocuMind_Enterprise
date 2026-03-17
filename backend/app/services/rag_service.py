815a7e7bb9b9a0b356d08ca72471f28f95b2b963
mokshsalecha68-del <mokshsalecha68@gmail.com>
2026-03-17 17:05:32 +0530

    Update rag_service.py

--- backend/app/services/rag_service.py
+++ backend/app/services/rag_service.py
@@ -1,1 +1,12 @@
+from app.services.vector_service import similarity_search
+from app.services.qwen_service import generate_answer_from_context
 
+def generate_answer(question: str, user_id: str):
+    docs = similarity_search(question, user_id)
+
+    if not docs:
+        return "No relevant information found."
+
+    context = "\n\n".join([doc.page_content for doc in docs])
+
+    return generate_answer_from_context(context, question)
